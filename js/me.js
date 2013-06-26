e7c1d3ac3d16bb8c072e5a5b1e5f8772ebff8133
ecx io <ecx.at.devices@gmail.com>
2013-06-26 09:46:29 +0200

    delete swipe effects

--- js/me.js
+++ js/me.js
@@ -1,6 +1,6 @@
-$( document ).on( "pageinit", ".swipeEffect", function() {
+$( document ).on( "pageinit", "#demo-page", function() {
 
-    $( document ).on( "swipeleft swiperight", ".swipeEffect", function( e ) {
+    $( document ).on( "swipeleft swiperight", "#demo-page", function( e ) {
         // We check if there is no open panel on the page because otherwise
         // a swipe to close the left panel would also open the right panel (and v.v.).
         // We do this by checking the data that the framework stores on the page element (panel: open).
@@ -33,14 +33,7 @@ $(document).ready(function() {
         //console.log(data);
 
         $.each(data, function(key, val) {
-            items.push('
-                <li>
-                    <a href="'+val['link']+'" rel="external">
-                        <img height="80px" src="'+val['thumbimg']+'">
-                        <h2 title="'+val['title']+'" >'+val['title']+'</h2>
-                        <p>'+val['description']+'</p>
-                    </a>
-                </li>');
+            items.push('<li><a href="'+val['link']+'" rel="external"><img height="80px" src="'+val['thumbimg']+'"><h2 title="'+val['title']+'" >'+val['title']+'</h2><p>'+val['description']+'</p></a></li>');
         });
 
         $("#dataList").html(items.join(''));
